c0f510c40c8314c2ecf2cf4c29788d96c47dd04b
Jim <b97201019@gmail.com>
2023-03-13 17:16:24 +0900

    feat: add tailwind and daisyui settings

---
+++ src/util/url.ts
@@ -0,0 +1,17 @@
+export async function checkURLIsValid(url: string): Promise<boolean> {
+  if (typeof url !== "string") return false;
+
+  try {
+    new URL(url);
+  } catch (_err) {
+    return false;
+  }
+
+  return fetch(url)
+    .then((response) => {
+      return response.status === 200;
+    })
+    .catch(() => {
+      return false;
+    });
+}
