359c38f9a06102a1aa88c0e5576aa656c8e359c0
Jim <b97201019@gmail.com>
2023-03-13 12:04:46 +0900

    feat: add basic setting

---
+++ src/model/Url.ts
@@ -0,0 +1,14 @@
+interface OpenGraphMetadata {
+  title: string;
+  description: string;
+  img: string; // the url of string
+}
+
+export interface URLContent {
+  originURL: string; // original url
+  shortenURL: string; // new url
+  times: number;
+  userID: string; // equal to the id of user
+  originOGM?: OpenGraphMetadata;
+  customOGM?: OpenGraphMetadata;
+}
---
+++ src/model/User.ts
@@ -0,0 +1,6 @@
+export interface User {
+  name: string;
+  account: string;
+  password: string;
+  id: string;
+}
