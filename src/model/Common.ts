7b47563f4a503bac4622662d0b2f4aa6f2ec834b
Jim <b97201019@gmail.com>
2023-03-16 17:28:33 +0900

    refactor: adjust some logic of api part

---
+++ src/model/Common.ts
@@ -0,0 +1,3 @@
+export interface ResponseContent {
+  message?: string;
+}
--- src/pages/api/[shortUrl].ts
+++ src/pages/api/[shortUrl].ts
@@ -1,43 +1,38 @@
-import { User } from "@component/model/User";
-import { getShortUrl } from "@component/util/hash";
+import { ResponseContent } from "@component/model/Common";
 import type { NextApiRequest, NextApiResponse } from "next";
 import sequelize, { UrlSequelize } from "../../server/db";
 
-type Data = {
-  shortUrl: string;
-};
-
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data | { message: string }>
+  res: NextApiResponse<ResponseContent>
 ) {
   try {
     await sequelize.authenticate();
     console.log("Connection has been established successfully.");
+
+    if (req.method === "GET") {
+      const { shortUrl } = req.query;
+
+      if (shortUrl) {
+        const result = await UrlSequelize.findOne({
+          where: {
+            shortUrl,
+          },
+        });
+        if (!result) {
+          res.redirect(303, "/");
+          return;
+        }
+        res.redirect(303, result.originUrl);
+      } else {
+        res.status(400).json({ message: "Nothing happen" });
+      }
+
+      // }
+    } else {
+      res.status(400).json({ message: "Invalid request method" });
+    }
   } catch (error) {
     console.error("Unable to connect to the database:", error);
-  }
-
-  if (req.method === "GET") {
-    const { shortUrl } = req.query;
-
-    if (shortUrl) {
-      const result = await UrlSequelize.findAll({
-        where: {
-          shortUrl,
-        },
-      });
-      if (result.length === 0) {
-        res.redirect(303, "/");
-        return;
-      }
-      res.redirect(303, result[0].originUrl);
-    } else {
-      res.status(400).json({ message: "Nothing happen" });
-    }
-
-    // }
-  } else {
-    res.status(400).json({ message: "Invalid request method" });
   }
 }
